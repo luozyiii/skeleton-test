63c9a81c51ac8746c4d79aeccea98d30b95ccee1
罗智毅 <luozhiyi@123>
2019-06-15 15:17:18 +0800

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>标题标题标题</p>
+    <p>标题标题标题</p>
+    <p>标题标题标题</p>
+    <p>标题标题标题</p>
+    <p>标题标题标题</p>
+    <p>标题标题标题</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
